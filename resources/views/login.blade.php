2504069d0a19930d37d6018ffcde9d8bb7ec8972
RafliHaikhal <rafli.haikhal@gmail.com>
2023-01-06 23:21:36 +0700

    ui login udah selesai

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,9 +2,39 @@
 @include('layout.header')
 
 <body>
-    <div class="body-container">
-        <div class="base bg-secondary">
+    <div class="login-body-container py-5">
+        <div class="login-body-content">
+            <div class="row">
+                <div class="col login-body-rightside">
+                    
+                </div>
+                <div class="col login-body-leftside py-5">
+                    <div class="login-gadgeton-text mb-3">
+                        <h1><span class="navbar-text text-black">Gadget</span><span class="navbar-text text-danger">On</span></h1>
+                    </div>
+                    <h5 class="login-signin-text mb-4">Sign in to your account</h5>
+                    <form action="" method="POST">
+                    <div class="form-item">
+                        <label for="name">Email Address</label>
+                        <input type="email" name="" id="">
+                    </div>
+                    <div class="form-item">
+                        <label for="name">Password</label>
+                        <input type="password" name="" id="">
+                    </div>
+                    <div class="form-item">
+                        <input type="checkbox" name="" id="">
+                        <label for="rememberme">Remember Me</label>
+                    </div>
+                    </form>
+                <button class="login-button"type="submit">
+                    Login
+                </button>
+                <p>Don't have an account? <a class="login-registernow"href="/register">Register here</a></p>
+                </div>
+            </div>
             
+
         </div>
     </div>
 </body>
